78e73886b05a0825b0e6948d3ade18caa31f07e4
Luis Osvaldo Cabrera García <l.osvaldocabrera@gmail.com>
2023-10-16 15:18:17 -0600

    2.Crear un componente tabla_usuarios

--- src/app/usuarios/usuarios.module.ts
+++ src/app/usuarios/usuarios.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TablaUsuariosComponent } from './tabla-usuarios/tabla-usuarios.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TablaUsuariosComponent
+  ],
+  exports: [
+    TablaUsuariosComponent
+  ],
   imports: [
     CommonModule
   ]
