e1a42a673313adece0baf30ef2e8a1a32fda206d
Luis Osvaldo Cabrera García <l.osvaldocabrera@gmail.com>
2023-10-16 15:04:01 -0600

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'capi_examen_front_Luis_Osvaldo_Cabrera_Garcia';
+}
